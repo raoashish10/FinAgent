72da0b201d127670142ab440ce6bd400dda94ca8
Ashish Rao <ashish@Ashishs-MacBook-Pro.local>
2024-12-15 23:51:07 -0800

    add postgres memory saver

--- agent.py
+++ agent.py
@@ -1,17 +1,21 @@
 from utils.nodes import agent, tool_node, should_continue
 from langgraph.graph import END, START, StateGraph, MessagesState
-from langgraph.checkpoint.memory import MemorySaver
 
-workflow = StateGraph(MessagesState)
-workflow.add_node("agent", agent)  # agent
-workflow.add_node("tools", tool_node)
 
-workflow.add_edge(START, "agent")
-workflow.add_conditional_edges(
-    "agent",
-    should_continue,
-)
-workflow.add_edge("tools", 'agent')
 
-checkpointer = MemorySaver()
-graph = workflow.compile(checkpointer=checkpointer)
+
+def get_graph(checkpointer):
+    workflow = StateGraph(MessagesState)
+    workflow.add_node("agent", agent)  # agent
+    workflow.add_node("tools", tool_node)
+
+    workflow.add_edge(START, "agent")
+    workflow.add_conditional_edges(
+        "agent",
+        should_continue,
+    )
+    workflow.add_edge("tools", 'agent')
+
+    # checkpointer = MemorySaver()
+    graph = workflow.compile(checkpointer=checkpointer)
+    return graph
--- main.py
+++ main.py
@@ -1,17 +1,35 @@
-from fastapi import FastAPI
-from agent import graph
-from langchain_core.messages import HumanMessage
 from dotenv import load_dotenv
+load_dotenv()
+from fastapi import FastAPI
+from agent import get_graph
+from langchain_core.messages import HumanMessage
+from psycopg_pool import ConnectionPool
 import os
+from langgraph.checkpoint.postgres import PostgresSaver
+from langgraph.checkpoint.memory import MemorySaver
 
 app = FastAPI()
-load_dotenv()
+
+connection_kwargs = {
+    "autocommit": True,
+    "prepare_threshold": 0,
+}
 
 @app.get("/")
 def query_llm(query: str) -> str:
-    final_state = graph.invoke(
-        {"messages": [HumanMessage(content=query)]},
-        config={"configurable": {"thread_id": 42}}
-    )
-    last_message = final_state['messages'][-1].content
-    return last_message
+    with ConnectionPool(
+    # Example configuration
+    conninfo=os.environ['DB_URI'],
+    max_size=20,
+    kwargs=connection_kwargs,
+    ) as pool:
+        checkpointer = PostgresSaver(pool)
+        # checkpointer = MemorySaver()
+        checkpointer.setup()
+        graph = get_graph(checkpointer)
+        final_state = graph.invoke(
+            {"messages": [HumanMessage(content=query)]},
+            config={"configurable": {"thread_id": "1"}}
+        )
+        last_message = final_state['messages'][-1].content
+        return last_message
